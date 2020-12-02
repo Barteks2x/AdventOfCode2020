f7fca424a7d4f23487e1bba4fc031eade045a656
Barteks2x <Barteks2x@gmail.com>
2020-12-02 18:07:51 +0100

    Simplify day2 code a bit more

--- src/main/java/day2/Main2.java
+++ src/main/java/day2/Main2.java
@@ -4,22 +4,22 @@ import java.nio.file.*;
 import java.util.stream.*;
 
 public class Main2 {
-
     record PasswordEntry(String password, char letter, int num1, int num2) {
+        boolean valid1() {
+            int count = password.length() - password.replaceAll("" + letter, "").length();
+            return count >= num1 && count <= num2;
+        }
+
+        boolean valid2() {
+            return (password.charAt(num1 - 1) == letter) ^ (password.charAt(num2 - 1) == letter);
+        }
     }
 
     // 620, 727
     public static void main(String[] args) throws Throwable {
-        var lines = Files.readAllLines(Paths.get("in2.txt")).stream()
-                .map(Main2::parseEntry).collect(Collectors.toList());
-        System.out.println(lines.stream().filter(entry -> {
-            int count = entry.password.length() - entry.password.replaceAll("" + entry.letter, "").length();
-            return count >= entry.num1 && count <= entry.num2;
-        }).count());
-        System.out.println(lines.stream().filter(entry ->
-                (entry.password.charAt(entry.num1 - 1) == entry.letter)
-                        ^ (entry.password.charAt(entry.num2 - 1) == entry.letter)
-        ).count());
+        var lines = Files.readAllLines(Paths.get("in2.txt")).stream().map(Main2::parseEntry).collect(Collectors.toList());
+        System.out.println(lines.stream().filter(PasswordEntry::valid1).count());
+        System.out.println(lines.stream().filter(PasswordEntry::valid2).count());
     }
 
     private static PasswordEntry parseEntry(String entry) {
