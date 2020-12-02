1a5dc8838a184dbd0e3eedbbc56da99ea3cc6ef2
Barteks2x <Barteks2x@gmail.com>
2020-12-02 18:01:09 +0100

    Simplify day2 code

--- src/main/java/day2/Main2.java
+++ src/main/java/day2/Main2.java
@@ -1,35 +1,30 @@
 package day2;
 
 import java.nio.file.*;
-import java.util.function.*;
+import java.util.stream.*;
 
 public class Main2 {
 
     record PasswordEntry(String password, char letter, int num1, int num2) {
     }
 
+    // 620, 727
     public static void main(String[] args) throws Throwable {
-        var lines = Files.readAllLines(Paths.get("in2.txt")).toArray(new String[0]);
-        System.out.println(countValidPasswords(lines, entry -> {
-                    int count = entry.password.length() - entry.password.replaceAll("" + entry.letter, "").length();
-                    return count >= entry.num1 && count <= entry.num2;
-                })
-        );
-        System.out.println(countValidPasswords(lines, entry ->
-                (entry.password.charAt(entry.num1 - 1) == entry.letter) ^
-                        (entry.password.charAt(entry.num2 - 1) == entry.letter))
-        );
+        var lines = Files.readAllLines(Paths.get("in2.txt")).stream()
+                .map(Main2::parseEntry).collect(Collectors.toList());
+        System.out.println(lines.stream().filter(entry -> {
+            int count = entry.password.length() - entry.password.replaceAll("" + entry.letter, "").length();
+            return count >= entry.num1 && count <= entry.num2;
+        }).count());
+        System.out.println(lines.stream().filter(entry ->
+                (entry.password.charAt(entry.num1 - 1) == entry.letter)
+                        ^ (entry.password.charAt(entry.num2 - 1) == entry.letter)
+        ).count());
     }
 
-    private static int countValidPasswords(String[] lines, Predicate<PasswordEntry> tester) {
-        int count = 0;
-        for (var line : lines) {
-            var parts = line.split("[\\s:]+");
-            var numbers = parts[0].split("-");
-            if (tester.test(new PasswordEntry(parts[2], parts[1].charAt(0), Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])))) {
-                count++;
-            }
-        }
-        return count;
+    private static PasswordEntry parseEntry(String entry) {
+        var parts = entry.split("[\\s:]+");
+        var numbers = parts[0].split("-");
+        return new PasswordEntry(parts[2], parts[1].charAt(0), Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
     }
 }
