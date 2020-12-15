61db80e84d6cff1628552c353110e2510b1c715f
Barteks2x <Barteks2x@gmail.com>
2020-12-15 22:36:56 +0100

    Optimzie day 15

--- src/main/java/day15/Main15.java
+++ src/main/java/day15/Main15.java
@@ -1,24 +1,25 @@
 package day15;
 
-import it.unimi.dsi.fastutil.ints.*;
-
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main15 {
     public static void main(String[] args) throws IOException {
+        long t = System.nanoTime();
         var in = Files.readAllLines(Paths.get("in15.txt")).stream().flatMap(x -> Arrays.stream(x.split(","))).mapToInt(Integer::parseInt).toArray();
-        Int2IntMap lastTurn = new Int2IntOpenHashMap();
+        int[] lastTurn = new int[30000000];
 
-        int lastNum = -1;
-        for (int i = 1; i <= 30000000; i++) {
-            int num = i <= in.length ? in[i - 1] : i - lastTurn.getOrDefault(lastNum, i);
-            lastTurn.put(lastNum, i);
+        int lastNum = in[0];
+        for (int i = 2; i <= 30000000; i++) {
+            int num = i <= in.length ? in[i - 1] : i - (lastTurn[lastNum] == 0 ? i : lastTurn[lastNum]);
+            lastTurn[lastNum] = i;
             lastNum = num;
             if (i == 2020 || i == 30000000) {
                 System.out.println(lastNum);
             }
         }
+        System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - t));
     }
 }
