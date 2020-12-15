837f847dddec21d92e36182a1361664395067afe
Barteks2x <Barteks2x@gmail.com>
2020-12-15 22:37:28 +0100

    Remove time measurement from day 15

--- src/main/java/day15/Main15.java
+++ src/main/java/day15/Main15.java
@@ -3,11 +3,9 @@ package day15;
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
-import java.util.concurrent.*;
 
 public class Main15 {
     public static void main(String[] args) throws IOException {
-        long t = System.nanoTime();
         var in = Files.readAllLines(Paths.get("in15.txt")).stream().flatMap(x -> Arrays.stream(x.split(","))).mapToInt(Integer::parseInt).toArray();
         int[] lastTurn = new int[30000000];
 
@@ -20,6 +18,5 @@ public class Main15 {
                 System.out.println(lastNum);
             }
         }
-        System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - t));
     }
 }
